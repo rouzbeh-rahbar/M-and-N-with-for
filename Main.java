f3d2c4d4a6d332d70181452d4f8b2269dcd46232
rouzbeh-rahbar <rouzbeh.rahbar4@gmail.com>
2021-01-04 22:47:41 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Number 1 : ");
+        int m=sc.nextInt();
+        System.out.print("Enter Number 2 : ");
+        int n=sc.nextInt();
+        for(int i=m; i<=n; i++){
+            System.out.println(i);
+
+        }
+
+
+    }
+}
